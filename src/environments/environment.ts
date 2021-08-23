21be1ee590ec4e1183b070901093379d5df1775f
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-08-23 07:27:28 +0530

    Auth service is all Done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA3ufhuMCbNIv6tfyftR5L2LdIQ_s8oCdQ',
+    authDomain: 'travelgram-angularapp-firebase.firebaseapp.com',
+    databaseURL:
+      'https://travelgram-angularapp-firebase-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'travelgram-angularapp-firebase',
+    storageBucket: 'travelgram-angularapp-firebase.appspot.com',
+    messagingSenderId: '1012683821849',
+    appId: '1:1012683821849:web:fa2a727fc3647681a2eb88',
+  },
 };
 
 /*
