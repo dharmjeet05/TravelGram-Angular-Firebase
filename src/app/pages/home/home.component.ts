1924162fcec70fa0d4acd81e52bd0c9a0e9d7a7a
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-09-09 08:06:32 +0530

    Display users in home page is all Done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  users = [];
+  posts = [];
 
-  constructor() { }
+  isLoading = false;
 
-  ngOnInit(): void {
+  constructor(private db: AngularFireDatabase, private toastr: ToastrService) {
+    this.isLoading = true;
+
+    // Get all users
+    db.object('/users')
+      .valueChanges()
+      .subscribe((obj) => {
+        if (obj) {
+          this.users = Object.values(obj);
+          this.isLoading = false;
+        } else {
+          toastr.error('No User Found');
+          this.users = [];
+          this.isLoading = false;
+        }
+      });
+
+    // Get all posts
+    db.object('/posts')
+      .valueChanges()
+      .subscribe((obj) => {
+        if (obj) {
+          this.posts = Object.values(obj).sort((a, b) => b.date - a.date);
+          this.isLoading = false;
+        } else {
+          toastr.error('No Post to Display');
+          this.posts = [];
+          this.isLoading = false;
+        }
+      });
   }
 
+  ngOnInit(): void {}
 }
