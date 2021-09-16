bde0ef560779ae5c12dbf490d59eb01bd1b4fd62
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-09-16 22:55:06 +0530

    firebase bug is Done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA3ufhuMCbNIv6tfyftR5L2LdIQ_s8oCdQ',
+    authDomain: 'travelgram-angularapp-firebase.firebaseapp.com',
+    databaseURL:
+      'https://travelgram-angularapp-firebase-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'travelgram-angularapp-firebase',
+    storageBucket: 'travelgram-angularapp-firebase.appspot.com',
+    messagingSenderId: '1012683821849',
+    appId: '1:1012683821849:web:fa2a727fc3647681a2eb88',
+  },
 };
